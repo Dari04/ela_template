1f0c93ac514ee34be31b9dcfab7b8b90b9d3b40c
Dari Patiño <ingrid.patino@kushkipagos.com>
2022-06-26 12:06:13 -0500

    template finished

--- src/components/navBar/NavbarResponsive.vue
+++ src/components/navBar/NavbarResponsive.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="columns is-mobile navbarResponsive">
-		<div class="column"><a href="#destination"><b-icon icon="book-open-variant" size="is-small mr-1"></b-icon><br>Description</a></div>
+		<div class="column"><a href="#itineraryDescription"><b-icon icon="book-open-variant" size="is-small mr-1"></b-icon><br>Description</a></div>
 		<div class="column"><a href="#itinerary"><b-icon icon="earth" size="is-small mr-1"></b-icon><br>Itinerary</a></div>
 		<div class="column"><a href="#accommodationsAndPrices"><b-icon icon="bed" size="is-small mr-1"></b-icon><br>Prices</a></div>
 		<div class="column"><a href="#contactUs"><b-icon icon="email" size="is-small mr-1"></b-icon><br>Contact Us</a></div>
